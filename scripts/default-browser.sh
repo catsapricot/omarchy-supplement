5e1e74cfc1a7bc0c4ac3c8aa973c8a81646078d9
catsapricot <catsapricot@gmail.com>
2026-04-15 20:32:18 +0700

    style: perubahan aturan default browser

--- scripts/default-browser.sh
+++ scripts/default-browser.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-xdg-settings set default-web-browser firefox.desktop
+DEFAULT_BROWSER=$(xdg-settings get default-web-browser)
+
+if [ "$DEFAULT_BROWSER" != "firefox.desktop" ]; then
+    xdg-settings set default-web-browser firefox.desktop
+fi
